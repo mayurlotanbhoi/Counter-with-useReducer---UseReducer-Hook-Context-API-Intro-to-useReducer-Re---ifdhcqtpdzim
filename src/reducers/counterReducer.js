e6703846fcf7b6e10d8bc9899a2874bf2976494e
mayur bhoi <mayurbhoi200@gmail.com>
2022-09-18 20:18:54 +0530

    Update counterReducer.js

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,5 +1,21 @@
 const counterReducer = (state,action) =>{
 
+console.log(state +" " + action)
+switch(action.type){
+
+        case "INCREMENT":
+
+        return state + 1;
+
+        case "DICREMENT":
+
+        return state - 1;
+
+        default:
+
+        return state;
+
+}
 }
 
 export {counterReducer}
